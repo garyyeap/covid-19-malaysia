b60396e752b8d36731d38be346427dbf898dda7c
Gary Yeap <contact@garyyeap.com>
2021-08-29 23:04:28 +0800

    Add deaths, tests and new cases charts

---
+++ epidemic.js
@@ -0,0 +1,138 @@
+import { init } from 'https://cdnjs.cloudflare.com/ajax/libs/echarts/5.1.2/echarts.esm.min.js';
+
+function combineTestsAndCasesData (testsData, casesData) {
+  casesData = [...casesData];
+  casesData.unshift({});
+
+  const diff = casesData.length - testsData.length;
+  return testsData.reduce(function (result, val, i) {
+    const { cases_new, cases_import, cases_recovered, date } = casesData[i + diff];
+    const totalCases = parseInt(cases_new) + parseInt(cases_import);
+    const totalTests = parseInt(val.pcr) + parseInt(val['rtk-ag']);
+    const sevenDaysTotal = casesData.slice(Math.max(i + diff - 6, diff), i + diff + 1).reduce(function (total, curr, i) {
+      return total + parseInt(curr.cases_new) + parseInt(curr.cases_import);
+    }, 0);
+
+    result.dates.push(date);
+    result.cases.push(cases_new);
+    result.casesImport.push(cases_import);
+    result.casesRecovered.push(cases_recovered);
+    result.pcr.push(val.pcr);
+    result.rtk.push(val['rtk-ag']);
+    result.positiveRate.push((totalCases / totalTests * 100).toFixed(2));
+    result.sevenDaysAverage.push(parseInt(sevenDaysTotal / 7));
+    return result;
+  }, {
+    pcr: [],
+    rtk: [],
+    cases: [],
+    casesImport: [],
+    casesRecovered: [],
+    dates: [],
+    positiveRate: [],
+    sevenDaysAverage: []
+  });
+}
+
+export default function (testsData, casesData, deathsData) {
+  const { pcr, rtk, cases, casesImport, casesRecovered, dates, positiveRate, sevenDaysAverage } = combineTestsAndCasesData(testsData, casesData);
+  const [deaths, deathDates] = deathsData.reduce(function (result, { date, deaths_new}) {
+    result[0].push(deaths_new);
+    result[1].push(date);
+
+    return result;
+  }, [[], []]);
+  
+  const testsAndCasesOptions = {
+    legend: {
+      data: ['PCR tests', 'RTK-AG tests', 'New cases', 'Cases import', 'Positive rate', '7 days average']
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        label: {
+          backgroundColor: '#6a7985'
+        }
+      }
+    },
+    xAxis: {
+      data: dates
+    },
+    yAxis: {},
+    dataZoom: [{
+      type: 'slider',
+      start: 0,
+      end: 100
+    }, 
+    {
+      start: 0,
+      end: 100
+    }],
+    series: [{
+      name: 'PCR tests',
+      type: 'bar',
+      stack: 'tests',
+      data: pcr
+    }, 
+    {
+      name: 'RTK-AG tests',
+      type: 'bar',
+      stack: 'tests',
+      data: rtk
+    },
+    {
+      name: 'New cases',
+      type: 'bar',
+      stack: 'cases',
+      data: cases
+    }, 
+    {
+      name: 'Cases import',
+      type: 'bar',
+      stack: 'cases',
+      data: casesImport,
+    },
+    {
+      name: 'Positive rate',
+      type: 'line',
+      data: positiveRate,
+      zlevel: 101,
+    },
+    {
+      name: '7 days average',
+      type: 'line',      
+      zlevel: 100,
+      data: sevenDaysAverage
+    }]
+  };
+
+  init(document.getElementById('tests-and-cases-timeline'), 'dark').setOption(testsAndCasesOptions);
+
+
+  const deathsOptions = {
+    tooltip: {
+      trigger: 'item'
+    },
+    xAxis: {
+      data: deathDates
+    },
+    yAxis: {},
+    dataZoom: [{
+      type: 'slider',
+      start: 0,
+      end: 100
+    }, 
+    {
+      start: 0,
+      end: 100
+    }],
+    series: [{
+      name: 'Deaths',
+      type: 'bar',
+      data: deaths
+    }]
+  }
+
+  init(document.getElementById('deaths-timeline'), 'dark').setOption(deathsOptions);
+}
--- main.js
+++ main.js
@@ -1,55 +1,103 @@
 import csv2JSON from './csv.js';
-import initChart from './vax-chart.js';
+import initVaxChart from './vaccine.js';
+import initEpidemicChart from './epidemic.js';
+
+function fetchCSV (url) {
+  return fetch(url).then(response => response.text()).then(text => csv2JSON(text));
+}
+
+function categoryByState (data) {
+  return data.reduce(function (result, val) {
+    if (!result[val.state]) result[val.state] = [];
+
+    result[val.state].push(val);
+
+    return result;
+  }, {});
+}
 
 (async function () {
-  function switchChart () {
-    if (selector.value === states[0]) {
-      initChart(vaxMalaysiaData, parseInt(populationData[0].pop_18));
+  function initCharts () {
+    const currentState = stateSelector.value;
+
+    document.title = categorySelector.value + '-' + currentState;
+
+    if (categorySelector.value === 'Vaccine') {
+      epidemicContainer.style.display = 'none';
+      vaxContainer.style.display = 'block';
+
+      if (currentState === states[0]) {
+        initVaxChart(vaxMalaysiaData, parseInt(populationData[0].pop_18));
+        return;
+      }
+
+      const population = parseInt(populationData[states.indexOf(currentState)].pop_18);
+      initVaxChart(vaxStatesData[currentState], population);
       return;
     }
 
-    const currentState = selector.value;
-    const population = parseInt(populationData[states.indexOf(currentState)].pop_18);
+    epidemicContainer.style.display = 'block';
+    vaxContainer.style.display = 'none';
 
-    initChart(vaxStatesData[currentState], population);
-    document.title = 'Vax-' + currentState;
+    if (currentState === states[0]) {
+      initEpidemicChart(testsMalaysiaData, casesMalaysiaData, deathsMalaysiaData);
+      return;
+    }
+    initEpidemicChart(testsStatesData[currentState], casesStatesData[currentState], deathsStatesData[currentState]);
   };
 
   const vaxStatesUrl = 'https://raw.githubusercontent.com/CITF-Malaysia/citf-public/main/vaccination/vax_state.csv';
   const vaxMalaysiaUrl = 'https://raw.githubusercontent.com/CITF-Malaysia/citf-public/main/vaccination/vax_malaysia.csv';
   const populationUrl = 'https://raw.githubusercontent.com/CITF-Malaysia/citf-public/main/static/population.csv';
-  const [ populationData, vaxMalaysiaData, vaxStatesData ] = await Promise.all([
-    fetch(populationUrl).then((response) => response.text()).then((text) => csv2JSON(text)),
-    fetch(vaxMalaysiaUrl).then((response) => response.text()).then((text) => csv2JSON(text)),
-    fetch(vaxStatesUrl).then((response) => response.text()).then((text) => {
-      return csv2JSON(text).reduce(function (result, val) {
-        if (!result[val.state]) result[val.state] = [];
-
-        result[val.state].push(val);
-
-        return result;
-      }, {});
-    })
+  const casesMalaysiaUrl = 'https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/cases_malaysia.csv';
+  const testsMalaysiaUrl = 'https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/tests_malaysia.csv';
+  const deathsMalaysiaUrl = 'https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/deaths_malaysia.csv';
+  const casesStetesUrl = 'https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/cases_state.csv';
+  const testsStetesUrl = 'https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/tests_state.csv';
+  const deathsStetesUrl = 'https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/deaths_state.csv';
+  const [ 
+    testsMalaysiaData,
+    casesMalaysiaData,
+    deathsMalaysiaData,
+    testsStatesData,
+    casesStatesData,
+    deathsStatesData,
+    populationData, 
+    vaxMalaysiaData, 
+    vaxStatesData 
+  ] = await Promise.all([
+    fetchCSV(testsMalaysiaUrl),
+    fetchCSV(casesMalaysiaUrl),
+    fetchCSV(deathsMalaysiaUrl),
+    fetchCSV(testsStetesUrl).then(categoryByState),
+    fetchCSV(casesStetesUrl).then(categoryByState),
+    fetchCSV(deathsStetesUrl).then(categoryByState),
+    fetchCSV(populationUrl),
+    fetchCSV(vaxMalaysiaUrl),
+    fetchCSV(vaxStatesUrl).then(categoryByState)
   ]);
   const mainContainer = document.getElementById('main-container');
+  const vaxContainer = document.getElementById('vax-container');
+  const epidemicContainer = document.getElementById('epidemic-container');
   const loadingCover = document.getElementById('loading-cover');
   const states = populationData.map(val => val.state);
-  const selector = document.getElementById('switch');
+  const stateSelector = document.getElementById('state-switch');
+  const categorySelector = document.getElementById('category-switch');
 
-  selector.onchange = switchChart;
-  selector.innerHTML = states.map(function (val) {
+  stateSelector.onchange = categorySelector.onchange = initCharts;
+  stateSelector.innerHTML = states.map(function (val) {
     return `<option value="${val}">${val}</option>`;
   }).join('');
 
   loadingCover.ontransitionend = function () {
     loadingCover.style.display = 'none';
     mainContainer.style.display = 'block';
-    customSelect('#switch');
-    switchChart();
+    customSelect('.switch');
+    initCharts();
   };
 
   window.onresize = function () {  
-    window.setTimeout(switchChart, 500);
+    window.setTimeout(initCharts, 500);
   };
 
   loadingCover.style.opacity = 0;
